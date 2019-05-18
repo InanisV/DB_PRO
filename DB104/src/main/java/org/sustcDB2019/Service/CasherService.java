690e8d0ff61fff50062eac6465820381dbe06761
Qing <newtypes@outlook.com>
2019-05-18 18:17:13 +0800

    xml change

--- DB104/src/main/java/org/sustcDB2019/Service/CasherService.java
+++ DB104/src/main/java/org/sustcDB2019/Service/CasherService.java
@@ -5,7 +5,7 @@ import org.apache.ibatis.session.SqlSession;
 public class CasherService {
     public int updateCasher(Casher casher){
         SqlSession sqlSession=DAOService.sqlSessionFactory.openSession();
-        CasherMapper mapper=sqlSession.getMapper(CasherMapper.class);//[add mapper] add casher's entity and mapper
+        CasherMapper mapper=sqlSession.getMapper(CasherMapper.class);//[add mapper] add casher's entity and mapper [Aready]
         mapper.updateByPrimaryKeySelective(casher);
         return 0;
     }
