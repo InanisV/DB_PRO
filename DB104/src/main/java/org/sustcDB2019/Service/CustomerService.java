386114d974ddf0a97d26e06b144fabd6fdb32191
natsudong <2289339056@qq.com>
2019-05-22 00:30:37 +0800

    0:30

--- DB104/src/main/java/org/sustcDB2019/Service/CustomerService.java
+++ DB104/src/main/java/org/sustcDB2019/Service/CustomerService.java
@@ -33,7 +33,7 @@ public class CustomerService extends UserService {
         ArrayList<Warehouse> warehouses = WarehouseService.getAllWarehouse();
         double R = 6371.0;
         int minWarehouseId = 0;
-        double minDistance = 0;
+        double minDistance = 9999999;
         for (int i = 0; i < warehouses.size(); i++) {
             double phi1 = (90 - customer.getCustomerLati().doubleValue()) * PI / 180, phi2 = (90 - warehouses.get(i).getWarehouseLati().doubleValue()) * PI / 180;
             double c = sin(phi1) * sin(phi2) * cos((customer.getCustomerLong().doubleValue() - warehouses.get(i).getWarehouseLong().doubleValue()) * PI / 180) + cos(phi1) *
